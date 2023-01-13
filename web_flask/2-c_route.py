68fee204a24822255ca599c42db7a2244ef75624
Heiswill <willtriger@gmail.com>
2023-01-13 07:54:00 -0800

    test

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-""" A script that starts a Flask web application. """
+""" A script that starts a flask web application"""
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Prints a message when / is called """
-    return "Hello HBNB!"
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ prints a message when /hbnb is called """
-    return "HBNB"
+    """ Prints a Message when /hbnb is called """
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_texts():
-    """ Prints a messages when /c/text is called """
+def c_is_fun(text):
+    """ Prints a Message when /c is called """
     return "C " + text.replace('_', ' ')
-
 
 if __name__ == "__main__":
     """ Main Function """
