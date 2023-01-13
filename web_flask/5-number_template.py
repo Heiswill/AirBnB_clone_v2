04d8dcf5c6c41015365d96cfea4d58dd20838cfa
Heiswill <willtriger@gmail.com>
2023-01-13 09:12:01 -0800

    test5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" A script that starts a flask web application"""
+""" A script that starts a flask web application """
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -31,14 +31,13 @@ def python_is_cool(text='is_cool'):
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def n_is_number(n):
-    """ Prints a Message when /number is called only if n is an integer"""
+    """ Prints a Message when /number is called only if n is an int"""
     return "{:d} is a number".format(n)
 
 
-@app.route('/number_template/int:<n>', strict_slashes=False)
-def htmlpage(n):
-    """ Display html page when /number_template is called
-    if only n is an integer """
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ display a HTML page only if n is an integer """
     return render_template('5-number.html', value=n)
 
 
