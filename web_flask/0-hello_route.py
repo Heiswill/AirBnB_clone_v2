0b54fcc402c6532e50543d32815db70e3ba6353e
Heiswill <willtriger@gmail.com>
2023-01-13 07:04:01 -0800

    test

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-
+""" A script that satrts a flask web application """
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes=False
 
-@app.route('/')
+
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return "Hello HBNB"
+    """ Prints a message when / is called """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
